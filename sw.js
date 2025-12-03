f3124b9ac8a2fab61a5dd859a3cfc91a0047de7f
Vaishali Gawai <gawaivaishali9423@gmail.com>
2025-12-04 01:16:55 +0530

    More Changes suggested by deepseek

--- sw.js
+++ sw.js
@@ -1,42 +1,13 @@
-const CACHE_NAME = 'youtube-pwa-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-// Install event
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+// Simple Service Worker
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installed');
 });
 
-// Activate event
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activated');
 });
 
-// Fetch event
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', function(event) {
+    // Just pass through, don't cache
+    event.respondWith(fetch(event.request));
 });
